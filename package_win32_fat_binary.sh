059b4e755d13699363db33d4c67af0a65f3a7404
Luis Lavena <luislavena@gmail.com>
2013-07-19 12:05:28 -0300

    Remove cross-compilation from specific tasks
    Prepare cross-compiled Rubies in one place and use them.

--- package_win32_fat_binary.sh
+++ package_win32_fat_binary.sh
@@ -9,9 +9,8 @@ cd '/vagrant'
 # passed in path of gem to be cross-compiled
 cd $1
 
+# need to use 1.8.7 for fat-binaries (1.9.3 can't cross-build 1.8.7)
 rvm use 1.8.7
 gem install bundler && bundle install
-bundle exec rake-compiler cross-ruby
-bundle exec rake-compiler cross-ruby VERSION=1.9.3-p448
 
 bundle exec rake cross native gem RUBY_CC_VERSION=1.8.7:1.9.3
