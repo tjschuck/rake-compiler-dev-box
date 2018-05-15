000ab69cf30b86a2db82e29fdb7a0f7cc84a045b
T.J. Schuck <tj@tjschuck.com>
2018-05-15 11:45:58 -0400

    Use xenial64 and install mingw-w64 via apt-get
    - xenial has `mingw-w64` in apt; trusty doesn’t
    
    - Why not bionic? xenial is still on libssl 1.0; bionic has jumped to 1.1.  Ruby 1.8 won’t build with 1.1, but does with 1.0.

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,48 +13,25 @@ if [[ $(nproc) -gt 1 ]] && ! grep -q "make -j" $home/.bash_profile; then
 fi
 
 apt-get -y update
-apt-get install -y curl git-core mingw32 default-jdk unzip
-
-# Download mingw-w64 compilers
-mingw32='i686-w64-mingw32-gcc-4.7.2-release-linux64_rubenvb.tar.xz'
-mingw64='x86_64-w64-mingw32-gcc-4.7.2-release-linux64_rubenvb.tar.xz'
-
-$as_vagrant 'mkdir -p ~/mingw'
-
-if [ ! -d "$home/mingw/mingw32/bin" ]; then
-  $as_vagrant "curl -L http://downloads.sourceforge.net/mingw-w64/$mingw32 -o ~/mingw/$mingw32"
-  $as_vagrant "tar -C ~/mingw -xf ~/mingw/$mingw32"
-fi
-
-if [ ! -d "$home/mingw/mingw64/bin" ]; then
-  $as_vagrant "curl -L http://downloads.sourceforge.net/mingw-w64/$mingw64 -o ~/mingw/$mingw64"
-  $as_vagrant "tar -C ~/mingw -xf ~/mingw/$mingw64"
-fi
+apt-get install -y curl git-core mingw-w64 default-jdk unzip
 
 # Install wrappers for strip commands
-if [ ! -f "$home/mingw/mingw32/bin/i686-w64-mingw32-strip.bin" ]; then
+if [ ! -f "/usr/bin/i686-w64-mingw32-strip.bin" ]; then
   echo "Install wrapper for i686-w64-mingw32-strip"
-  mv $home/mingw/mingw32/bin/i686-w64-mingw32-strip $home/mingw/mingw32/bin/i686-w64-mingw32-strip.bin
-  cp /vagrant/bin/strip_wrapper $home/mingw/mingw32/bin/i686-w64-mingw32-strip
+  mv /usr/bin/i686-w64-mingw32-strip /usr/bin/i686-w64-mingw32-strip.bin
+  cp /vagrant/bin/strip_wrapper /usr/bin/i686-w64-mingw32-strip
 fi
 
-if [ ! -f "$home/mingw/mingw64/bin/x86_64-w64-mingw32-strip.bin" ]; then
+if [ ! -f "/usr/bin/x86_64-w64-mingw32-strip.bin" ]; then
   echo "Install wrapper for x86_64-w64-mingw32-strip"
-  mv $home/mingw/mingw64/bin/x86_64-w64-mingw32-strip $home/mingw/mingw64/bin/x86_64-w64-mingw32-strip.bin
-  cp /vagrant/bin/strip_wrapper $home/mingw/mingw64/bin/x86_64-w64-mingw32-strip
+  mv /usr/bin/x86_64-w64-mingw32-strip /usr/bin/x86_64-w64-mingw32-strip.bin
+  cp /vagrant/bin/strip_wrapper /usr/bin/x86_64-w64-mingw32-strip
 fi
 
 if [ ! -f "/usr/bin/i586-mingw32msvc-strip.bin" ]; then
   echo "Install wrapper for i586-mingw32msvc-strip"
   mv /usr/bin/i586-mingw32msvc-strip /usr/bin/i586-mingw32msvc-strip.bin
   cp /vagrant/bin/strip_wrapper /usr/bin/i586-mingw32msvc-strip
-fi
-
-# add mingw-w64 to the PATH
-mingw_w64_paths="$home/mingw/mingw32/bin:$home/mingw/mingw64/bin"
-
-if ! grep -q $mingw_w64_paths $home/.bash_profile; then
-  echo "export PATH=\$PATH:$mingw_w64_paths" >> $home/.bash_profile
 fi
 
 # do not generate documentation for gems
